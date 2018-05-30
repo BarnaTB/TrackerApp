48b73d47530d82a078c403e8512605a44aed7309
BarnaTB <barnabastb2@gmail.com>
2018-05-30 19:43:36 +0300

    Modify test file

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -8,21 +8,34 @@ class Requests(unittest.TestCase):
         self.tester = tracker.app.test_client()
 
     def test_user_gets_requests(self):
-        # this tests if the user can get the requests that have already been created
-        request = dict(requesttype='requesttype',
-                       category='category', details='details')
+        pass
+    #     # this tests if the user can get the requests that have already been created
+    #     request = dict(requesttype='requesttype',
+    #                    category='category', details='details')
 
-        expected_message = 'Request view succesful!'
+    #     reply = self.tester.get('/app/v1/users/requests')
 
-        reply = self.tester.get('/app/v1/users/requests')
+    #     self.assertEqual(reply.status_code, 201)
 
-        self.assertEqual(reply.status_code, 201)
+    # def test_user_create_requests(self):
+    #     # this tests if a user can create a request
+    #     # create the request data
+    #     user_request = ({
+    #         'requesttype': 'replace',
+    #         'category': 'water',
+    #         'details': 'The pipe to the sink is overflowing'
+    #     })
+    #     response = self.tester.post(
+    #         '/app/v1/users/requests', content_type='application/json', data=json.dumps(user_request))
+    #     reply = json.loads(response.data.decode())
 
-    def test_user_create_requests(self):
-        request = dict(requesttype='requesttype', category='category', details='details')
-        reply = self.tester.post('/app/v1/users/requests', content_type='application/json', data=json.dumps(request))
+    #     self.assertEquals(reply['status'], 'OK')
+    #     self.assertEquals(reply['message'], 'Request created successfully')
+    #     self.assertEquals(reply.status_code, 201)
 
-        self.assertEqual(reply.status_code, 201)
+    # def test_user_modify_request(self):
+    #     #this tests that a user can modify a request
+    #     request = dict()
 
 
 if __name__ == '__main__':
