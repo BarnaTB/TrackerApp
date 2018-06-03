2855a168f38dfbc3c64545455ae67f2b87568d9b
BarnaTB <barnabastb2@gmail.com>
2018-06-03 16:54:36 +0300

    Remove create_request_data class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, jsonify
 class User_request:
     def __init__(self, _id, requesttype, category, details):
         self._id = _id
@@ -16,3 +17,14 @@ class User_request:
 
     def get_details(self):
         return self.details
+
+
+# class Request_data(User_request):
+#     def create_request_data(self, _id, requesttype, category, details):
+#         request_data = request.get_json()
+
+#         req_type = request_data.get('requesttype')
+#         req_category = request_data.get('category')
+#         req_details = request_data.get('details')
+
+#         return request_data
