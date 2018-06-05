b8a3ebe84f1c50182da4bd92fe6a0b693049c3bc
BarnaTB <barnabastb2@gmail.com>
2018-06-05 14:24:27 +0300

    Add getters

--- app/manage.py
+++ app/manage.py
@@ -1,12 +1,21 @@
 import psycopg2
 
 class my_db:
-    def __init__(self, db_name, user, password, cur, conn):
-        self.db_name = db_name
-        self.user_name = user
+    def __init__(self, dbname, user, password, conn):
+        self.dbname = dbname
+        self.user = user
         self.password = password
         self.conn = psycopg2.connect("dbname=tracker_db user=user_1 password=database")
         self.cur = conn.cursor()
     
-    def create_table():
-        pass
+    def get_dbname(self):
+        return self.dbname
+
+    def get_user(self):
+        return self.user
+
+    def get_password(self):
+        return self.password
+
+    def get_conn(self):
+        return self.conn
