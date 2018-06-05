f528b0ed324a23f8d6b933fb9cd20f31af44f191
BarnaTB <barnabastb2@gmail.com>
2018-06-05 14:06:35 +0300

    Refactor if statements

--- app/views.py
+++ app/views.py
@@ -63,11 +63,11 @@ def create_request():
     # check if each required field is present in the data
     if not requesttype:
         return jsonify({'message': 'Missing request type'}), 400
-    if not category:
+    elif not category:
         return jsonify({'message': 'Missing request category'}), 400
-    if not details:
+    elif not details:
         return jsonify({'message': 'Missing request details'}), 400
-    if not _id:
+    elif not _id:
         return jsonify({'message': 'Missing request id'}), 400
 
     # create a new request as an instance of the User_request class
