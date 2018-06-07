ffa2e261d611309dc52c09b9cc2de451ad3ac11e
BarnaTB <barnabastb2@gmail.com>
2018-06-07 03:01:20 +0300

    Remove unused code

--- run.py
+++ run.py
@@ -1,21 +1,2 @@
 from app import app
 app.run(debug=True)
-# from flask import Flask, request, jsonify
-# import json
-
-# app = Flask(__name__)
-
-# #from app import app
-# #from flask import Flask
-
-
-
-# #from app import views
-
-# import os
-# #from app.db import My_db
-
-# from app import views
-# with app.app_context():
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(debug=True, host="0.0.0.0", port=port)
